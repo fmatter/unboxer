0712eb493d43e34b112aca2a1a9905db416443aa
Florian Matter <florianmatter@gmail.com>
2022-11-25 11:45:23 -0800

    validate cldf in tests

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -2,7 +2,7 @@
 """
 from click.testing import CliRunner
 from box2csv.cli import corpus
-
+from pycldf import Dataset
 
 def test_toolbox(data, tmp_path):
     runner = CliRunner()
@@ -21,3 +21,5 @@ def test_toolbox(data, tmp_path):
     # extract_corpus(data / "yekwana.txt", conf, tmp_path, cldf=True)
     assert (tmp_path / "yekwana.csv").is_file()
     assert (tmp_path / "cldf" / "examples.csv").is_file()
+    ds = Dataset.from_metadata(tmp_path / "cldf" / "metadata.json")
+    assert ds.validate()
